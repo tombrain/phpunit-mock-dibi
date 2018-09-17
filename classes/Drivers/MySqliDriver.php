5756ebcb6b755af439ce4cf98e7e5e12c781037f
Korney Czukowski <carbofos@seznam.cz>
2018-09-17 06:33:12 +0200

    Move everything from MySql Driver Trait to MySqli Driver

--- classes/Drivers/MySqliDriver.php
+++ classes/Drivers/MySqliDriver.php
@@ -3,6 +3,7 @@ namespace Cz\PHPUnit\MockDibi\Drivers;
 
 use Cz\PHPUnit\SQL,
     Dibi\Drivers,
+    Dibi\NotImplementedException,
     mysqli;
 
 /**
@@ -17,7 +18,56 @@ class MySqliDriver extends Drivers\MySqliDriver implements
 {
     use MockQueryConnectionTrait;
     use MockQueryDriverTrait;
-    use MySqlDriverTrait;
+
+    /**
+     * @param  mixed  $savepoint
+     */
+    public function begin(?string $savepoint = NULL): void
+    {
+        $this->addExecutedQuery($savepoint ? "SAVEPOINT $savepoint" : 'START TRANSACTION');
+    }
+
+    /**
+     * @param  mixed  $savepoint
+     */
+    public function commit(?string $savepoint = NULL): void
+    {
+        $this->addExecutedQuery($savepoint ? "RELEASE SAVEPOINT $savepoint" : 'COMMIT');
+    }
+
+    /**
+     * @param  mixed  $savepoint
+     */
+    public function rollback(?string $savepoint = NULL): void
+    {
+        $this->addExecutedQuery($savepoint ? "ROLLBACK TO SAVEPOINT $savepoint" : 'ROLLBACK');
+    }
+
+    /**
+     * @param   string  $value
+     * @return  string
+     */
+    public function escapeBinary(string $value): string
+    {
+        return MySqlEscapingHelper::escapeBinary($value);
+    }
+
+    /**
+     * @param   string  $value
+     * @return  string
+     */
+    public function escapeText(string $value): string
+    {
+        return MySqlEscapingHelper::escapeText($value);
+    }
+
+    /**
+     * @throws  NotImplementedException
+     */
+    public function getInfo(): array
+    {
+        throw new NotImplementedException('No query info for mock DB connection');
+    }
 
     /**
      * @return  MySqliResult
--- tests/Drivers/MySqliDriverTest.php
+++ tests/Drivers/MySqliDriverTest.php
@@ -12,6 +12,94 @@ use Dibi\NotImplementedException;
 class MySqliDriverTest extends Testcase
 {
     /**
+     * @dataProvider  provideBegin
+     */
+    public function testBegin($savepoint, $expected)
+    {
+        $object = $this->createObject();
+        $object->begin($savepoint);
+        $this->assertExecutedQuery($object, $expected);
+    }
+
+    public function provideBegin()
+    {
+        return [
+            [NULL, 'START TRANSACTION'],
+            ['s1', 'SAVEPOINT s1'],
+        ];
+    }
+
+    /**
+     * @dataProvider  provideCommit
+     */
+    public function testCommit($savepoint, $expected)
+    {
+        $object = $this->createObject();
+        $object->commit($savepoint);
+        $this->assertExecutedQuery($object, $expected);
+    }
+
+    public function provideCommit()
+    {
+        return [
+            [NULL, 'COMMIT'],
+            ['s1', 'RELEASE SAVEPOINT s1'],
+        ];
+    }
+
+    /**
+     * @dataProvider  provideRollback
+     */
+    public function testRollback($savepoint, $expected)
+    {
+        $object = $this->createObject();
+        $object->rollback($savepoint);
+        $this->assertExecutedQuery($object, $expected);
+    }
+
+    public function provideRollback()
+    {
+        return [
+            [NULL, 'ROLLBACK'],
+            ['s1', 'ROLLBACK TO SAVEPOINT s1'],
+        ];
+    }
+
+    /**
+     * @dataProvider  provideEscapeBinary
+     */
+    public function testEscapeBinary($value, $expected)
+    {
+        $object = $this->createObject();
+        $actual = $object->escapeBinary($value);
+        $this->assertSame($expected, $actual);
+    }
+
+    public function provideEscapeBinary()
+    {
+        return [
+            ["value", "_binary'value'"],
+        ];
+    }
+
+    /**
+     * @dataProvider  provideEscapeText
+     */
+    public function testEscapeText($value, $expected)
+    {
+        $object = $this->createObject();
+        $actual = $object->escapeText($value);
+        $this->assertSame($expected, $actual);
+    }
+
+    public function provideEscapeText()
+    {
+        return [
+            ["value", "'value'"],
+        ];
+    }
+
+    /**
      * @dataProvider  provideNotImplementedMethods
      */
     public function testNotImplementedMethods($method)
